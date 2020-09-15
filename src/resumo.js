7e6899926450d545c497674bd01ee09b21e159a9
André Lara <66387919+andrelara2002@users.noreply.github.com>
2020-09-14 22:14:25 -0300

    Atualizando codigos

--- src/resumo.js
+++ src/resumo.js
@@ -5,18 +5,18 @@ const listCustosF = document.getElementById('custosf-list')
 const listCustosV = document.getElementById('custosv-list')
 
 const state = {
-    ganhos: 0,
-    perdas: 0,
-    expectativa: 0,
+    ganhos: 1000,
+    perdas: 900,
+    expectativa: "--",
     lucroAtual: 0,
-    previsaoFixa:0,
-    previsaoVariavel:0,
+    previsaoFixa: 0,
+    previsaoVariavel: 0,
     metas: {
         nome: [],
         valorTotal: [],
         valorMensal: [],
         ValorDario: [],
-        tempoemMeses:[]
+        tempoemMeses: []
     },
     investimentos: {
         nome: [],
@@ -30,8 +30,60 @@ const state = {
         valorMensal: []
     },
     custosVariaveis: {
-        nome :[],
+        nome: [],
         valorMensal: []
     }
-    
+
 }
+
+render = function () {
+    listResumo.innerHTML = ""
+    renderResumo("Meus ganhos", state.ganhos, "ganhos")
+    renderResumo("Meus gastos", state.perdas, "gastos")
+    calcExpectativa()
+    renderResumo("Expectativa", state.expectativa, "none")
+    getIdColor("ganhos")
+    getIdColor("gastos")
+}
+
+renderResumo = (label, valor, id) => {
+    const listElement = document.createElement('li')
+    const labelElement = document.createElement('strong')
+    const labelText = document.createTextNode(label)
+    const valorElement = document.createElement('p')
+    const valorText = document.createTextNode(valor)
+    labelElement.setAttribute("id", id)
+    labelElement.style.lineHeight = "0"
+
+    labelElement.appendChild(labelText)
+    valorElement.appendChild(valorText)
+
+    labelElement.appendChild(valorElement)
+    listElement.appendChild(labelElement)
+
+    listResumo.appendChild(listElement)
+}
+getIdColor = (id) => {
+    const strongElement = document.getElementById(id)
+
+    if (id === "ganhos") {
+        strongElement.style.fontWeight = "900"
+        strongElement.style.color = "lightgreen"
+    }
+    else {
+        strongElement.style.fontWeight = "900"
+        strongElement.style.color = "red"
+    }
+}
+
+calcExpectativa = () =>{
+    const result = state.ganhos - state.perdas
+    if (state.ganhos > state.perdas){
+        state.expectativa = "Dentro do esperado"
+    }
+    else {
+        state.expectativa = "Débito negativo"
+    }
+}
+
+render();
